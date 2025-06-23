b964cdadaab2ce1d3f2f57297b61c2e6fd55253e
USSA <ussa9905@gmail.com>
2025-06-23 11:33:57 +0900

    Complete

---
+++ HW05/Intermediate/Build/Win64/x64/TESTEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "D:/Git/HW05/HW05/Intermediate/Build/Win64/x64/TESTEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ HW05/Intermediate/Build/Win64/x64/TESTEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "D:/Git/HW05/HW05/Intermediate/Build/Win64/x64/TESTEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
